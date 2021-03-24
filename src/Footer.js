ec60192aa62fb21d0502d205d96e806d2fabb819
gurutricon <guru.ramu@triconinfotech.com>
2021-03-24 22:43:15 +0530

    gitignore

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="footer">
-        <h3>© 2020 Abodbnb, Inc. All rights reserved. </h3>
+        <h3>© 2020 Airbnb, Inc. All rights reserved. </h3>
         <h3>Project based on AIRBNB UI. </h3>
         <h5>Privacy · Terms · Sitemap</h5>
       </div>
